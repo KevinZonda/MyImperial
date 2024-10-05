0c82c2def6052d04ab248996b2edf54537100cf9
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2024-10-05 02:14:07 +0100

    w

--- src/Weather.tsx
+++ src/Weather.tsx
@@ -10,11 +10,11 @@ export const WeatherBanner = () => {
     const rain = willRain(data)
     return <div>
         <p>
-        <span style={{fontWeight: 'bold'}}>Weather @ Imperial College: </span>
+            <span style={{ fontWeight: 'bold' }}>Weather @ Imperial College: </span>
+            W: {getWeatherCode(data.current.weatherCode)};
+            {rain.WillRain && `🌧️ will rain max ${rain.H} mm/h;`}
             T: {data.current.temperature2m.toFixed(1)}°C (H: {data.daily.temperature2mMax[0].toFixed(1)}°C, L: {data.daily.temperature2mMin[0].toFixed(1)}°C);
-            R: {data.current.rain.toFixed(0)} mm/h;
-            W: {getWeatherCode(data.current.weatherCode)}
-            {rain.WillRain && `; 🌧️ will rain max ${rain.H} mm/h`}
+            R: {data.current.rain.toFixed(0)} mm/h
         </p>
     </div>
 }
