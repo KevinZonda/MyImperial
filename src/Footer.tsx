a8c32524a03de850c111aa6cf46ae95392edbc4c
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2024-10-06 00:28:09 +0100

    better footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,11 +1,15 @@
 import { COLOUR_IMPERIAL_BLUE } from "./const/Colour"
 
 export const Footer = () => {
+    
     return (
         <div style={{backgroundColor: COLOUR_IMPERIAL_BLUE, color: 'white', paddingLeft: 32, paddingRight: 32, paddingTop: '21px', paddingBottom: '18px'}}>
+            <p style={{marginTop: 0, marginBottom: 0}}>KevinZonda :: MyImperial</p>
             <p style={{marginTop: 0, marginBottom: 0}}>Copyright &copy; 2024 KevinZonda (Xiang Shi)</p>
-            <p style={{marginTop: 0, marginBottom: 0}}>All your data is stored locally and never passed on to anyone.</p>
-            <p style={{marginTop: 0, marginBottom: 0}}>KevinZonda :: MyImperial is a utility written by KevinZonda to help Imperial College London DoC students access their education system. This platform is not affiliated to Imperial College London.</p>
+            <p style={{marginTop: 0, marginBottom: 0}}>{import.meta.env.GIT_HASH}@{import.meta.env.GIT_BRANCH} {import.meta.env.GIT_DATE}</p>
+            <p style={{marginTop: 0, marginBottom: 0}}>This service is to help Imperial College London DoC students access their education system simpler, and is not affiliated to Imperial College London.</p>
+            <p style={{marginTop: 0, marginBottom: 0}}>This is a free service provided as is. No guarantees are made about the availability or correctness of the data. Use at your own risk.</p>
+            <p style={{marginTop: 0, marginBottom: 0}}>All data is stored locally and never passed on to anyone.</p>
         </div>
     )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { execSync } from 'child_process';
+
+const commitHash = execSync('git rev-parse --short HEAD').toString().trim();
+const commitDate = execSync('git log -1 --format=%cd').toString().trim();
+const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    "import.meta.env.GIT_HASH": JSON.stringify(commitHash),
+    "import.meta.env.GIT_DATE": JSON.stringify(commitDate),
+    "import.meta.env.GIT_BRANCH": JSON.stringify(branch),
+  },
   plugins: [react()],
 })
