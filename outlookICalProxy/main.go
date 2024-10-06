13d8fb4bd306cff862da8e8a3a83408abae537a3
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2024-10-06 01:05:17 +0100

    fix

--- outlookICalProxy/main.go
+++ outlookICalProxy/main.go
@@ -1,27 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
-	"strings"
 
 	"github.com/KevinZonda/GoX/pkg/panicx"
+	"github.com/KevinZonda/GoX/pkg/stringx"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
-func URL(id, token, calendar, ics string) string {
-
-	return strings.Join([]string{
-		"https://outlook.office365.com/owa/calendar",
-		id,
-		token,
-		calendar,
-		ics,
-	}, "/")
-}
 func main() {
 	if !Config.Debug {
 		gin.SetMode(gin.ReleaseMode)
@@ -40,40 +29,37 @@ func main() {
 		c.String(200, "pong")
 	})
 
-	reachcalendar := proxyGen("reachcalendar.ics")
-	calendar := proxyGen("calendar.ics")
-
-	engine.GET("/calendar/:id/:token/:calendar/reachcalendar.ics", reachcalendar)
-	engine.GET("/calendar/:id/:token/:calendar/calendar.ics", calendar)
-	engine.GET("/calendar/:id/:token/:calendar", calendar)
-	engine.GET("/owa/calendar/:id/:token/:calendar/reachcalendar.ics", reachcalendar)
-	engine.GET("/owa/calendar/:id/:token/:calendar/calendar.ics", calendar)
-	engine.GET("/owa/calendar/:id/:token/:calendar", calendar)
+	engine.GET("/calendar/:id/:token/:calendar/reachcalendar.ics", proxy)
+	engine.GET("/calendar/:id/:token/calendar.ics", proxy)
+	engine.GET("/owa/calendar/:id/:token/:calendar/reachcalendar.ics", proxy)
+	engine.GET("/owa/calendar/:id/:token/calendar.ics", proxy)
 	err := engine.Run(Config.ListenAddr)
 	panicx.NotNilErr(err)
 }
 
-func proxyGen(ics string) func(c *gin.Context) {
-	return func(c *gin.Context) {
-		id := c.Param("id")
-		token := c.Param("token")
-		calendar := c.Param("calendar")
-		outlookUrl := URL(id, token, calendar, ics)
-		fmt.Println(outlookUrl)
-		remote, err := url.Parse(outlookUrl)
-		if err != nil {
-			c.String(500, "Failed to parse url")
-			return
-		}
-		px := httputil.NewSingleHostReverseProxy(remote)
-		px.Director = func(req *http.Request) {
-			req.Header = c.Request.Header
-			req.Host = remote.Host
-			req.URL.Scheme = remote.Scheme
-			req.URL.Host = remote.Host
-			req.URL.Path = remote.Path
-		}
-
-		px.ServeHTTP(c.Writer, c.Request)
+func proxy(c *gin.Context) {
+	id := c.Param("id")
+	token := c.Param("token")
+	calendar := c.Param("calendar")
+	tail := "reachcalendar.ics"
+	if calendar == "" {
+		tail = "calendar.ics"
 	}
+	outlookUrl := stringx.JoinNotEmpty("/", "https://outlook.office365.com/owa/calendar", id, token, calendar, tail)
+	remote, err := url.Parse(outlookUrl)
+	if err != nil {
+		c.String(500, "Failed to parse url")
+		return
+	}
+	px := httputil.NewSingleHostReverseProxy(remote)
+	px.Director = func(req *http.Request) {
+		req.Header = c.Request.Header
+		req.Host = remote.Host
+		req.URL.Scheme = remote.Scheme
+		req.URL.Host = remote.Host
+		req.URL.Path = remote.Path
+	}
+
+	px.ServeHTTP(c.Writer, c.Request)
+
 }
